79ca467636b87e74ecdba0d650c7018381d31bf3
Abdelrahman Ahmed <Abdelrahman.Ahmed@alqemam.com>
2024-07-10 13:18:24 +0300

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-@Component({
+import { FormsModule,ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import {CommonModule} from '@angular/common' 
+import { CLOSING } from '../../node_modules/@types/ws';
+import { RegExpLiteral } from '../../node_modules/@types/estree';
+@Component({ 
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test';
+
+  noneSpecials:any=/^[a-zA-Z0-9 ]*$/;
+  Numbers:any = /^[0-9]*$/
+  ArabicCodes:any = /[\u0600-\u06FF\u0750-\u077F]/
+  res:boolean = true;
+
+  inputOnePressed(e:Event){
+    if(!this.noneSpecials.test((e as KeyboardEvent).key)){
+        alert('Not valid Input special characters not allowed');
+        e.preventDefault()
+    }
+  }
+  inputTwoPressed(e:Event){
+    if(this.Numbers.test((e as KeyboardEvent).key)){
+      alert('Not valid Input Numbers not allowed');
+      e.preventDefault()
+  }
+  }
+  noArabicChars(e:Event){
+    if(this.ArabicCodes.test((e as KeyboardEvent).key)){
+      alert('Not valid Arabic characters not allowed');
+      e.preventDefault()
+  }
+  }
+
 }
