850db0812c1d8f7820714ace6a9dfa8604b20895
Abdelrahman Ahmed <Abdelrahman.Ahmed@alqemam.com>
2024-07-10 16:07:44 +0300

    another way to achive validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,25 +16,19 @@ export class AppComponent {
   noneSpecials:any=/^[a-zA-Z0-9 ]*$/;
   Numbers:any = /^[0-9]*$/
   ArabicCodes:any = /[\u0600-\u06FF\u0750-\u077F]/
-  res:boolean = true;
 
-  inputOnePressed(e:Event){
-    if(!this.noneSpecials.test((e as KeyboardEvent).key)){
-        alert('Not valid Input special characters not allowed');
-        e.preventDefault()
-    }
-  }
-  inputTwoPressed(e:Event){
-    if(this.Numbers.test((e as KeyboardEvent).key)){
-      alert('Not valid Input Numbers not allowed');
-      e.preventDefault()
-  }
-  }
-  noArabicChars(e:Event){
-    if(this.ArabicCodes.test((e as KeyboardEvent).key)){
-      alert('Not valid Arabic characters not allowed');
-      e.preventDefault()
-  }
-  }
+
+  myFormGroup = new FormGroup({
+    firstInput:new FormControl(
+      '',Validators.pattern(/^[a-zA-Z0-9 ]*$/)
+    ),
+    secondInput:new FormControl(
+      '',Validators.pattern(/^[^0-9]*$/)
+    ),
+    thirdInput:new FormControl(
+    '',Validators.pattern(/^[^\u0600-\u06FF\u0750-\u077F]*$/)
+    )
+  })
+
 
 }
